12b0a3a97fdeb4263d345b3e0f8845d24c974f61
jiyeong <jiyeongstar@gmail.com>
2022-04-09 05:05:00 +0900

    지영 FE-3 과제완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
 import "./App.css";
-import Person from "./components/Person";
-// const people = ["지영", "지인", "지은"];
-const people = [
-  { name: "지영", age: 24 },
-  { name: "수연", age: 24 },
-  { name: "유경", age: 24 },
-];
+import { useState, useEffect, useRef, useMemo, useCallback, memo } from "react";
+//memo,callback최적화
 function App() {
+  const [number, setNumber] = useState<number>(0);
+  function onClickUpButton() {
+    setNumber((prev) => prev + 1);
+
+    //비동기적으로 실행되기 때문
+    //리액트는 virtual dom 이기 때문에
+  }
+  function onClickDownButton() {
+    setNumber((prev) => prev - 1);
+  }
+  // useEffect(() => {
+  //   console.log(number);
+  //   //컴포넌트마운트
+  //   //의존성 배열의 값이 바뀔때
+
+  //   const timeout = setTimeout(() => {
+  //     console.log("hello");
+  //   }, 1000);
+  //   console.log(buttonRef.current);
+  //   // if (!buttonRef.current) return;
+  //   // buttonRef.current.addEventListener("click", () => {
+  //   //   setNumber((prev) => prev - 1);
+  //   // });
+  //   //컴포넌트언마운트
+  //   //청소할 때
+  //   // return () => {
+  //   //   // clearTimeout(timeout);
+  //   //   // console.log("끝");
+  //   // };
+  // }, []);
+  // const buttonRef = useRef<HTMLButtonElement>(null); //자바스크립트코드에서 Html을 사용하고 싶을 때?
+
   return (
     <div>
       <h1>DogVelopers 22</h1>
-      <h1>안녕하세요 저는 지영입니다</h1>
-      {people.map((e) => {
-        return <Person name={e.name} age={e.age} />;
-      })}
+
+      {number % 2 === 0 ? (
+        <h2 className="even">{number}</h2>
+      ) : (
+        <h2 className="odd">{number}</h2>
+      )}
+      <button onClick={onClickUpButton}>up</button>
+      <button onClick={onClickDownButton}>down</button>
+      {/* <button ref={buttonRef}>down</button> */}
     </div>
   );
 }
